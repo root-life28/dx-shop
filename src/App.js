ecfcea3ad07c3591c31acdba3192a7f344271512
Muzahidul <bekon28@outlook.com>
2022-02-08 21:05:21 +0600

    form and button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInaAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
   <Routes>
     <Route path='/' element={<HomePage/>}/>
     <Route path='/shop' element={<ShopPage />}/>
+    <Route path='/signin' element={<SignInaAndSignUpPage />}/>
   </Routes>
     </div>
   );
