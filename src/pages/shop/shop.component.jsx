baab852c8bcdf0f7c68ecee478b57a646fd1bfcd
Muzahidul <bekon28@outlook.com>
2022-02-18 10:46:23 +0600

    updated directory sections data and shop data using redux and reselect

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/menu-item/collection-preview/collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-class ShopPage extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            collections:SHOP_DATA
-        };
-    }
-
-    render(){
-        const {collections}=this.state;
-        return(
+const ShopPage=({collections})=>(
+ 
             <div className="shop-page">
                 {
                     collections.map(({id,...otherCollectionProps})=>(
@@ -22,8 +14,8 @@ class ShopPage extends React.Component {
                     ))
                 }
             </div>
-        );
-    }
-}
-
-export default ShopPage;
+);
+const mapStateToProps=createStructuredSelector({
+    collections:selectCollections
+})
+export default connect(mapStateToProps) (ShopPage);
